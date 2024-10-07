d32396e00477feb5283c174178a0e4e0be5cee9a
Thiago Silva Ferreira <thiagosfsjc@gmail.com>
2024-10-07 07:52:55 -0300

    chore: make dark ui

--- src/app/Messages/MessageListItem.tsx
+++ src/app/Messages/MessageListItem.tsx
@@ -6,17 +6,22 @@ type Props = {
 
 export function MessageListItem({ message }: Props) {
   return (
-    <div className="p-4 rounded-lg bg-slate-100 flex flex-col gap-4">
-      <div className="flex flex-col gap-1 text-xs">
+    <div className="p-4 rounded-md bg-slate-900 flex flex-col gap-4">
+      <div className="flex flex-col gap-1 text-xs text-slate-500">
         <div>uuid: {message.uuid}</div>
         <div>from: {message.senderUuid}</div>
         <div>to: {message.receiverUuid}</div>
         <div>{message.createdAt?.toISOString()}</div>
       </div>
-      <div className="">
-        <div className="break-words font-semibold">
-          {message.senderEncryptedMessage}
-        </div>
+      <div className="break-words">
+        <blockquote className="text-xl italic font-semibold text-slate-900 dark:text-white">
+          <svg className="w-8 h-8 text-slate-400 dark:text-slate-600 mb-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 14">
+            <path d="M6 0H2a2 2 0 0 0-2 2v4a2 2 0 0 0 2 2h4v1a3 3 0 0 1-3 3H2a1 1 0 0 0 0 2h1a5.006 5.006 0 0 0 5-5V2a2 2 0 0 0-2-2Zm10 0h-4a2 2 0 0 0-2 2v4a2 2 0 0 0 2 2h4v1a3 3 0 0 1-3 3h-1a1 1 0 0 0 0 2h1a5.006 5.006 0 0 0 5-5V2a2 2 0 0 0-2-2Z" />
+          </svg>
+          <p>
+            {message.senderEncryptedMessage}
+          </p>
+        </blockquote>
       </div>
     </div>
   )
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-slate-800 text-slate-300`}
       >
         {children}
       </body>
