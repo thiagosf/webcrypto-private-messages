67148c6d46f5157f4eb65979af37fcb02a41a6fd
Thiago Silva Ferreira <thiagosfsjc@gmail.com>
2024-10-08 07:59:40 -0300

    chore: decrypt messages

--- src/app/Messages/MessageList.tsx
+++ src/app/Messages/MessageList.tsx
@@ -4,12 +4,15 @@ import { useEffect, useState } from 'react'
 
 import { Message } from '@/models'
 import { useKeys } from '@/app/hooks/useKeys'
-import { MessagesService } from '@/services'
+import { DecryptionService, MessagesService } from '@/services'
 import { MessageListItem } from '@/app/Messages/MessageListItem'
+
+type DecryptedMessages = { [key: string]: string }
 
 export function MessageList() {
   const { keyPair } = useKeys()
   const [messages, setMessages] = useState<Array<Message>>([])
+  const [decryptedMessages, setDecryptedMessages] = useState<DecryptedMessages>({})
 
   async function loadMessages(keyPair: CryptoKeyPair) {
     const messageService = new MessagesService(keyPair)
@@ -21,6 +24,35 @@ export function MessageList() {
     if (keyPair) loadMessages(keyPair)
   }, [keyPair])
 
+  useEffect(() => {
+    async function decryptMessages() {
+      if (messages.length > 0 && keyPair?.privateKey) {
+        setDecryptedMessages(
+          (
+            await Promise.all(
+              messages.map(async (message) => {
+                let decryptedMessage = await new DecryptionService(keyPair.privateKey).decrypt(message.senderEncryptedMessage)
+                if (DecryptionService.SECRET_MESSAGE === decryptedMessage) {
+                  decryptedMessage = await new DecryptionService(keyPair.privateKey).decrypt(message.receiverEncryptedMessage)
+                }
+
+                return { message, decryptedMessage }
+              })
+            )
+          ).reduce((acc, item) => {
+            if (item.message.uuid) {
+              acc[item.message.uuid] = item.decryptedMessage
+            }
+
+            return acc
+          }, {} as DecryptedMessages)
+        )
+      }
+    }
+
+    decryptMessages()
+  }, [messages, keyPair])
+
   return (
     <div className="flex flex-col gap-4">
       {messages.map((message) => {
@@ -28,6 +60,7 @@ export function MessageList() {
           <MessageListItem
             key={message.uuid}
             message={message}
+            decryptedMessage={decryptedMessages[message.uuid!] || '...'}
           />
         )
       })}
