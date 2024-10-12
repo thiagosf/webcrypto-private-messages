95ffba374f1c875a82f18a3afa9855de19827589
Thiago Silva Ferreira <thiagosfsjc@gmail.com>
2024-10-12 16:08:07 -0300

    chore: create message WIP

--- src/services/MessagesService.ts
+++ src/services/MessagesService.ts
@@ -1,9 +1,19 @@
 import { Message } from '@/models'
 
 import list from '@/mocks/messages/list.json'
+import { NewMessageDto } from '@/dtos'
 
 export class MessagesService {
   async load(): Promise<Array<Message>> {
     return Promise.resolve(list.map(message => new Message({ ...message })))
+  }
+
+  async create(payload: NewMessageDto): Promise<void> {
+    const response = await fetch('/api/messages', {
+      method: 'POST',
+      body: JSON.stringify(payload)
+    })
+    const data = await response.json()
+    console.log(data)
   }
 }
