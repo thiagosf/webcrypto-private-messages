df3f0efa67f9b9a6e32aa773351b4e1ca8a5fd17
Thiago Silva Ferreira <thiagosfsjc@gmail.com>
2024-10-12 11:23:33 -0300

    chore: basic layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,19 @@ import { MessageList } from '@/app/Messages'
 
 export default function Home() {
   return (
-    <div className="m-10">
-      <MessageList />
+    <div className="m-10 flex gap-10">
+      <div className="shrink-0 w-64">
+        <h1 className="font-semibold">web crypto api</h1>
+        <p>private messages</p>
+        <hr className="border border-slate-700 my-6" />
+        <div className="flex flex-col gap-4">
+          <p>It&apos;s a <a href="https://developer.mozilla.org/en-US/docs/Web/API/Web_Crypto_API" className="border-b-4 border-slate-700 hover:border-slate-600 hover:text-white" target="_blank">Web Crypto API</a> experiment to encrypt/decrypt messages using public and private keys.</p>
+          <p>Only the author and the receiver of the message can read it.</p>
+        </div>
+      </div>
+      <div className="grow">
+        <MessageList />
+      </div>
     </div>
   )
 }
