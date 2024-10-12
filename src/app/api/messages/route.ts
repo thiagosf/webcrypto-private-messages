681960d8ecf89decd9c7d252579723e72b7194f2
Thiago Silva Ferreira <thiagosfsjc@gmail.com>
2024-10-12 16:07:47 -0300

    chore: implement create message route

--- src/app/api/messages/route.ts
+++ src/app/api/messages/route.ts
@@ -1,8 +1,10 @@
+import { randomUUID } from 'node:crypto'
 import { NextResponse } from 'next/server'
 
 import list from '@/mocks/messages/list.json'
 
 import { Message } from '@/models'
+import { insertMessage } from '@/helpers/db'
 
 export async function GET() {
   const messages = list.map(message => new Message(message))
@@ -10,8 +12,23 @@ export async function GET() {
   return NextResponse.json({ messages }, { status: 200 })
 }
 
-export async function POST() {
-  return NextResponse.json({ message: {} }, { status: 201 })
+export async function POST(request: Request) {
+  try {
+    const payload = await request.json()
+    const message = new Message({
+      uuid: randomUUID(),
+      senderUuid: payload.senderUuid,
+      receiverUuid: payload.receiverUuid,
+      receiverEncryptedMessage: payload.receiverEncryptedMessage,
+      senderEncryptedMessage: payload.senderEncryptedMessage,
+      createdAt: new Date().toISOString(),
+    })
+    await insertMessage(message)
+
+    return NextResponse.json({ success: true }, { status: 201 })
+  } catch (error) {
+    return NextResponse.json({ success: false, error: (error as Error).message }, { status: 400 })
+  }
 }
 
 export async function DELETE() {
