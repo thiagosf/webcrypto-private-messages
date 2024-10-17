89aa617a3182912c15453675b525507d06202fe3
Thiago Silva Ferreira <thiagosfsjc@gmail.com>
2024-10-17 07:38:00 -0300

    chore: remove users controller

--- src/controllers/UsersController.ts
+++
@@ -1,9 +0,0 @@
-export class UsersController {
-  async generateAuthCode(publicKey: string): Promise<string> {
-    return `${publicKey}123`
-  }
-
-  async validateAuthCode(authCode: string, decryptedAuthCode: string): Promise<boolean> {
-    return authCode === decryptedAuthCode
-  }
-}
