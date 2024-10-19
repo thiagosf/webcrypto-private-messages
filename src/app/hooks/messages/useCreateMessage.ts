0e5e9e7d8926eb10001e096e387e21c2b55f8b0c
Thiago Silva Ferreira <thiagosfsjc@gmail.com>
2024-10-19 18:01:59 -0300

    chore: get user uuid before creating message

--- src/app/hooks/messages/useCreateMessage.ts
+++ src/app/hooks/messages/useCreateMessage.ts
@@ -1,24 +1,27 @@
 import { useKeys } from '@/app/hooks/useKeys'
+import { useCreateUser } from '@/app/hooks/users'
 import { NewMessageDto } from '@/dtos'
-import { generateUuid } from '@/helpers'
+import { exportKey } from '@/helpers'
 import { Message } from '@/models'
 import { EncryptionService, MessagesService } from '@/services'
 
 export function useCreateMessage() {
   const { keyPair } = useKeys()
+  const { createOrFindUserUuid } = useCreateUser()
 
   async function createMessage(payload: NewMessageDto) {
     if (!keyPair) return
 
+    const publicKey = await exportKey(keyPair.publicKey)
+    const senderUuid = await createOrFindUserUuid(publicKey.n!)
     const newMessage = new Message({
-      senderUuid: generateUuid(),
+      senderUuid,
       receiverUuid: payload.receiverUUID,
       senderEncryptedMessage: await new EncryptionService(keyPair.publicKey).encrypt(payload.message!),
       receiverEncryptedMessage: await new EncryptionService(keyPair.publicKey).encrypt(payload.message!),
     })
     const messageService = new MessagesService()
-    const result = await messageService.create(newMessage)
-    console.log('> createMessage', result, payload)
+    await messageService.create(newMessage)
   }
 
   return { createMessage }
