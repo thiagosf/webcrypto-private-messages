f64f3a85f574c2b89271a0aea93d39ede991b98d
Thiago Silva Ferreira <thiagosfsjc@gmail.com>
2024-10-19 19:18:48 -0300

    chore: add findPublicKeyByUuid to UsersService

--- src/services/UsersService.ts
+++ src/services/UsersService.ts
@@ -5,6 +5,13 @@ type UserResult = {
   }
 }
 
+type FindPublicKeyResult = {
+  success: boolean,
+  data: {
+    publicKey: string
+  }
+}
+
 export class UsersService {
   async create(publicKey: string): Promise<string> {
     const response = await fetch('/api/users', {
@@ -17,5 +24,18 @@ export class UsersService {
     if (!data.success) throw new Error('Error to create user')
 
     return data.data.uuid
+  }
+
+  async findPublicKeyByUuid(uuid: string): Promise<string | undefined> {
+    const response = await fetch('/api/users/find-public-key-by-uuid', {
+      method: 'POST',
+      body: JSON.stringify({
+        uuid
+      })
+    })
+    const data = (await response.json()) as FindPublicKeyResult
+    if (!data.success) return
+
+    return data.data.publicKey
   }
 }
