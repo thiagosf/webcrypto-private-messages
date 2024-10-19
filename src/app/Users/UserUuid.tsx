ddded2ba40b4062f0aa0586282fb85f1741e6e73
Thiago Silva Ferreira <thiagosfsjc@gmail.com>
2024-10-19 19:20:10 -0300

    chore: add UserUuid component

---
+++ src/app/Users/UserUuid.tsx
@@ -0,0 +1,31 @@
+import { useEffect, useState } from 'react'
+
+import { useKeys } from '@/app/hooks'
+import { useCreateUser } from '@/app/hooks/users'
+import { exportKey } from '@/helpers'
+
+export function UserUuid() {
+  const { keyPair } = useKeys()
+  const { createOrFindUserUuid } = useCreateUser()
+  const [uuid, setUuid] = useState('')
+
+  async function loadUserUuid() {
+    const uuid = await createOrFindUserUuid(await exportKey(keyPair!.publicKey))
+    if (uuid) setUuid(uuid)
+  }
+
+  useEffect(() => {
+    if (keyPair?.publicKey) loadUserUuid()
+  }, [keyPair])
+
+  return (
+    <div className="flex flex-col gap-4">
+      <div>Your UUID:</div>
+      <input
+        readOnly
+        className="bg-slate-900 px-4 py-3 rounded-md outline-none"
+        value={uuid}
+      />
+    </div>
+  )
+}
