b6ddbb249722311771c4ce8f0af5263267709a65
Thiago Silva Ferreira <thiagosfsjc@gmail.com>
2024-10-19 18:02:23 -0300

    refactor: rename variables

--- src/app/Messages/NewMessageForm.tsx
+++ src/app/Messages/NewMessageForm.tsx
@@ -11,24 +11,24 @@ import { textToEmojis } from '@/helpers'
 const MAX_MESSAGE_TEXT_LENGTH = 190
 
 type Props = {
-  onSubmit: (newMessage: NewMessageDto) => unknown
+  onSubmit: (newMessageDto: NewMessageDto) => unknown
 }
 
 export function NewMessageForm({ onSubmit }: Props) {
   const { keyPair } = useKeys()
   const [encryptedMessagePreview, setEncryptedMessagePreview] = useState('')
-  const [newMessage, setNewMessage] = useState<NewMessageDto>({})
+  const [newMessageDto, setNewMessageDto] = useState<NewMessageDto>({})
 
   function handleSubmit() {
-    onSubmit(newMessage)
-    setNewMessage({})
+    onSubmit(newMessageDto)
+    setNewMessageDto({})
   }
 
   function handleChange<T extends HTMLInputElement | HTMLTextAreaElement>(field: keyof NewMessageDto) {
     return (event: ChangeEvent<T>): void => {
       const value = event.target.value
 
-      setNewMessage({ ...newMessage, [field]: value })
+      setNewMessageDto({ ...newMessageDto, [field]: value })
     }
   }
 
@@ -38,8 +38,8 @@ export function NewMessageForm({ onSubmit }: Props) {
   }
 
   useEffect(() => {
-    if (newMessage.message && keyPair) encryptMessage(newMessage.message, keyPair)
-  }, [newMessage.message, keyPair])
+    if (newMessageDto.message && keyPair) encryptMessage(newMessageDto.message, keyPair)
+  }, [newMessageDto.message, keyPair])
 
   return (
     <div className="flex gap-8 p-8 bg-slate-700 rounded-md">
@@ -49,7 +49,7 @@ export function NewMessageForm({ onSubmit }: Props) {
           <input
             placeholder="Receiver UUID"
             className="bg-transparent border border-slate-500 rounded-sm px-4 py-2 outline-none text-lg transition-colors focus:border-highlight-500"
-            value={newMessage.receiverUUID ?? ''}
+            value={newMessageDto.receiverUUID ?? ''}
             onChange={handleChange<HTMLInputElement>('receiverUUID')}
           />
           <div className="flex flex-col gap-2">
@@ -58,9 +58,9 @@ export function NewMessageForm({ onSubmit }: Props) {
               className="bg-transparent border border-slate-500 rounded-sm px-4 py-2 outline-none text-2xl transition-colors focus:border-highlight-500 resize-none"
               onChange={handleChange<HTMLTextAreaElement>('message')}
               maxLength={MAX_MESSAGE_TEXT_LENGTH}
-              value={newMessage.message || ''}
+              value={newMessageDto.message || ''}
             ></textarea>
-            <p>({MAX_MESSAGE_TEXT_LENGTH - (newMessage.message || '').length} characters missing)</p>
+            <p>({MAX_MESSAGE_TEXT_LENGTH - (newMessageDto.message || '').length} characters missing)</p>
           </div>
           <button
             className="bg-slate-800 rounded-sm p-4 text-lg font-extrabold outline-none transition-colors hover:bg-highlight-500 hover:text-slate-800"
@@ -71,7 +71,7 @@ export function NewMessageForm({ onSubmit }: Props) {
       <div className="flex-1 flex flex-col gap-2">
         <p className="font-extrabold">preview:</p>
         <div className="">
-          {newMessage.message && textToEmojis(encryptedMessagePreview)}
+          {newMessageDto.message && textToEmojis(encryptedMessagePreview)}
         </div>
       </div>
     </div>
