3fb03ff4538192799503133524c6d7137b22efb8
Thiago Silva Ferreira <thiagosfsjc@gmail.com>
2024-10-19 15:40:24 -0300

    chore: add create user route

--- src/app/api/users/create/route.ts
+++ src/app/api/users/create/route.ts
@@ -5,9 +5,9 @@ import { UsersController } from '@/controllers'
 export async function POST(request: Request) {
   try {
     const payload = await request.json()
-    const authCode = await new UsersController().generateAuthCode(payload.publicKey)
+    const userUuid = await new UsersController().create(payload.publicKey)
 
-    return NextResponse.json({ success: true, data: { authCode } }, { status: 200 })
+    return NextResponse.json({ success: true, data: { userUuid } }, { status: 200 })
   } catch (error) {
     console.log('> error to create message', error)
 
