ad643fb5189babbe9e42207b6caf92f12fea4075
Thiago Silva Ferreira <thiagosfsjc@gmail.com>
2024-10-26 15:25:56 -0300

    chore: use date type

--- src/repositories/MessageRepository.ts
+++ src/repositories/MessageRepository.ts
@@ -20,8 +20,8 @@ export class MessageRepository extends BaseRepository {
     let rows: Array<QueryResultRow> = []
     const limit = 2
     const maxCreatedAt = params.maxCreatedAt
-      ? params.maxCreatedAt.toISOString()
-      : new Date(new Date().getFullYear() + 1, 0, 0).toISOString()
+      ? params.maxCreatedAt
+      : new Date(new Date().getFullYear() + 1, 0, 0)
     const lastUuid = params.lastUuid ?? crypto.randomUUID()
 
     if (params.userUuid) {
